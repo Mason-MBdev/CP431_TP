ee086c5937e863b758a468d0fbfe908c14871d83
TannerBell1 <belltanner25@gmail.com>
2025-03-26 20:50:43 -0400

    Update multiplication.c
    lil boiler plate code

--- multiplication.c
+++ multiplication.c
@@ -1,1 +1,15 @@
+#include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  MPI_Init(&argc, argv);
+  int world_size, world_rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &rank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &size);
+
+
+
+  
+
+  
